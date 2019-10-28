4899c956f48ab6f902e991089ff6eac42a4fd8b2
traviszeng <traviszeng7@gmail.com>
2019-10-28 22:39:34 +0800

    add boost related

--- SharedDataInThreads.cpp
+++ SharedDataInThreads.cpp
@@ -158,3 +158,179 @@ public:
 		swap(r.detail,l.detail);
 	}
 };
+
+//std::unique_lock是可移动的，但不可赋值的类型。
+std::unique_lock<mutex> get_lock() {
+	extern std::mutex some_mutex;
+	std::unique_lock<mutex> lk(some_mutex);
+	//prepare_data();
+	return lk;
+}
+
+void process_data() {
+	unique_lock<mutex> lk(get_lock());  //编译器自动调用移动构造函数
+	//do_something();
+}
+
+void get_and_process_data() {
+	mutex mu;
+	unique_lock<mutex> my_lock(mu);
+	//some_class data_to_process = get_next_data_chunk();
+	my_lock.unlock(); // 1 不要让锁住的互斥量越过process()函数的调用
+	//result_type result = process(data_to_process);
+	my_lock.lock(); // 2 为了写入数据，对互斥量再次上锁
+	//write_result(data_to_process, result);
+}
+
+//比较操作符中一次锁住一个互斥量
+class Y
+{
+private:
+	int some_detail;
+	mutable std::mutex m;
+	int get_detail() const
+	{
+		std::lock_guard<std::mutex> lock_a(m); // 1
+		return some_detail;
+	}
+public:
+	Y(int sd) :some_detail(sd) {}
+	friend bool operator==(Y const& lhs, Y const& rhs)
+	{
+		if (&lhs == &rhs)
+			return true;
+		int const lhs_value = lhs.get_detail(); // 2
+		int const rhs_value = rhs.get_detail(); // 3
+		return lhs_value == rhs_value; // 4
+	}
+};
+
+//可能在2 3后 左右值会被修改，此时只是保证==为某一时刻的状态
+
+/**
+保护共享数据的初始化过程
+*/
+
+//延迟初始化：每一个操作都需要先对源进行检查，
+//为了了解数据是否被初始化，然后在其使用前决定，数据是否需要初始化
+//单线程版本
+shared_ptr<some_data> resource_ptr;
+void lazyInit() {
+	if (!resource_ptr) {
+		resource_ptr.reset(new some_data);//转为多线程时需要保护，这是因为每个线程必须等待互斥量，为了确定数据源已经初始化了。
+	}
+	resource_ptr->do_something;
+}
+
+//使用一个互斥量的延迟初始化(线程安全)过程
+std::mutex resource_mutex;
+void lazyInit()
+{
+	std::unique_lock<std::mutex> lk(resource_mutex); // 所有线程在此序列化
+	if (!resource_ptr)
+	{
+		resource_ptr.reset(new some_data); // 只有初始化过程需要保护
+	}
+	lk.unlock();
+	resource_ptr->do_something();
+}
+
+//双重检查锁模式
+/**
+外部的读取锁①没有与内部的
+写入锁进行同步③。因此就会产生条件竞争
+*/
+void undefined_behaviour_with_double_checked_locking()
+{
+	if (!resource_ptr) // 1
+	{
+		std::lock_guard<std::mutex> lk(resource_mutex);
+		if (!resource_ptr) // 2
+		{
+			resource_ptr.reset(new some_data); // 3
+		}
+	}
+	resource_ptr->do_something(); // 4
+}
+
+
+/*
+使用std::call_once和std::once_flag来处理
+比起锁住互斥量，并显式的检查指针，每个线程只需要使用 std::call_once 
+在 std::call_once 的结束时，就能安全的知道指针已经被其他的线程初始化了。
+*/
+std::once_flag resource_flag;
+void init_resource() {
+	resource_ptr.reset(new some_data);
+}
+
+void init_with_call_once() {
+	std::call_once(resource_flag, init_resource); //完整进行一次初始化
+	resource_ptr->do_something();
+}
+
+// 使用 std::call_once 作为类成员的延迟初始化(线程安全)
+/**
+第一个调用send_data()①或receive_data()③的线程完成初始化过程。
+使用成员函数open_connection()去初始化数据
+*/
+class X
+{
+private:
+	connection_info connection_details;
+	connection_handle connection;
+	std::once_flag connection_init_flag;
+	void open_connection()
+	{
+		connection = connection_manager.open(connection_details);
+	}
+public:
+	X(connection_info const& connection_details_) :
+		connection_details(connection_details_)
+	{}
+	void send_data(data_packet const& data) // 1
+	{
+		std::call_once(connection_init_flag, &X::open_connection, this); // 2
+		connection.send_data(data);
+	}
+	data_packet receive_data() // 3
+	{
+		std::call_once(connection_init_flag, &X::open_connection, this); // 2
+		return connection.receive_data();
+	}
+};
+/**
+用 boost::shared_lock<boost::shared_mutex> 获取共享访问权。这与使用 std::unique_lock 一
+样，除非多线要在同时得到同一个 boost::shared_mutex 上有共享锁。唯一的限制就是，当任
+一线程拥有一个共享锁时，这个线程就会尝试获取一个独占锁，直到其他线程放弃他们的
+锁；同样的，当任一线程拥有一个独占锁是，其他线程就无法获得共享锁或独占锁，直到第
+一个线程放弃其拥有的锁。
+*/
+// 使用 boost::shared_mutex 对数据结构进行保护
+#include <map>
+#include <string>
+#include <mutex>
+#include <boost/thread/shared_mutex.hpp>
+
+class dns_entry {};
+
+class dns_cache {
+	std::map<string, dns_entry> entries;
+	mutable boost::shared_mutex entry_mutex;
+public:
+	/**使用了 boost::shared_lock<> 实例来保护其共享和只读权限①；
+	这就使得，多线程可以同时调用find_entry()，且不会出错*/
+	dns_entry find_entry(string const& domain) const {
+		boost::shared_lock<boost::shared_mutex> lk(entry_mutex); //1
+		std::map<string, dns_entry>::const_iterator  const it = entries.find(domain);
+		return (it == entries.end()) ? dns_entry() : it->second;
+	}
+	/**当表格需要更新时②，为其提供独占访问权限；
+	在update_or_add_entry()函数调用时，独占锁会阻止其他线程对数据结构进行修改，并且这些线程在这时，也不能调用find_entry()。*/
+	void update_or_add_entry(std::string const& domain,dns_entry const& dns_details)
+	{
+		std::lock_guard<boost::shared_mutex> lk(entry_mutex); // 2
+		entries[domain] = dns_details;
+	}
+
+};
