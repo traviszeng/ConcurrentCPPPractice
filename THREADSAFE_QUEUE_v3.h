4f676a764d7e5c560ac7c298c5c71e990a55ac27
traviszeng <traviszeng7@gmail.com>
2019-11-03 19:54:35 +0800

    add single thread version queue

--- THREADSAFE_QUEUE_v3.h
+++ THREADSAFE_QUEUE_v3.h
@@ -1,4 +1,52 @@
 #pragma once
+using namespace std;
+//用单链表实现的单线程版本的queue
+template<typename T>
+class queue{
+private:
+	struct node{
+		T data;
+		std::unique_ptr<node> next;
+		node(T data_):data(std::move(data_)){}
+	};
+
+	std::unique<node> head;//1
+	node* tail;	//2
+public:
+	queue(){}
+	queue& operator=(const queue& other)=delete;
+	queue(const queue& other) = delete;
+
+	std::shared_ptr<T> try_pop(){
+		if(!head){
+			return std::shared_ptr<T>();
+		}
+		std::shared_ptr<T> const res(std::make_shared<T>(std::move(head->data)));
+
+		std::unique_ptr<T> const old_head = std::move(head);
+		head = std::move(old_head->next);//3
+		return res;
+	}
+
+	void push(T new_val){
+		std::unique_ptr<node> p(new node(std::move(new_val)));
+		node* const new_tail = p.get();
+
+		if(tail){
+			tail->next = std::move(p); //4
+		}
+		else
+		{
+			head = std::move(p); //5
+		}
+		tail = new_tail;
+		
+	}
+
+
+};
+
+
 template<typename T>
 class threadsafe_queue
 {
